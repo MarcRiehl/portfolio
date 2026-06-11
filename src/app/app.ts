eff6681c36ae9ac5f0536d15fda9de7cf4d2d59f
MarcRiehl <210008956+MarcRiehl@users.noreply.github.com>
2026-06-11 09:52:12 +0200

    feat: app.ts, fonts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { AboutMe } from './components/about-me/about-me';
+import { SkillSet } from './components/skill-set/skill-set';
+import { FeaturedProjects } from './components/featured-projects/featured-projects';
+import { Feed } from './components/feed/feed';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, AboutMe, SkillSet, FeaturedProjects, Feed, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
