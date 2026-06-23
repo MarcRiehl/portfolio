41259f762153a2f59ac8bdd54d853fa8ab38dcc8
MarcRiehl <210008956+MarcRiehl@users.noreply.github.com>
2026-06-23 12:28:22 +0200

    feat: contact-form getter style: button contact-form

--- src/app/components/contact-form/contact-form.ts
+++ src/app/components/contact-form/contact-form.ts
@@ -8,25 +8,46 @@ import { RouterLink } from "@angular/router";
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.scss',
 })
+
 export class ContactForm {
   userForm = new FormGroup({
     name: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(4)]
+      validators: [Validators.required, Validators.minLength(4)],
+      updateOn: 'blur'
     }),
     email: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(4), Validators.email]
+      validators: [Validators.required, Validators.email],
+      updateOn: 'blur'
     }),
-    inquiry: new FormControl('', {
-      validators: [Validators.required]
-    }), 
+    message: new FormControl('', {
+      validators: [Validators.required],
+      updateOn: 'blur'
+    }),
     privacyPolicy: new FormControl(false, {
       validators: [Validators.requiredTrue]
     })
   });
 
   onSubmit() {
-  if (this.userForm.valid) {
-    console.log(this.userForm.value);
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+    }
   }
-}
+
+  get name() {
+    return this.userForm.controls.name;
+  }
+
+  get email() {
+    return this.userForm.controls.email;
+  }
+
+  get message() {
+    return this.userForm.controls.message;
+  }
+
+  get privacyPolicy() {
+    return this.userForm.controls.privacyPolicy;
+  }
+
 }
